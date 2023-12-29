567c2e5ba5ae049533f3d9efb9efb3b20f8b99e4
AugustRush923 <augustrush923@163.com>
2023-12-29 15:23:41 +0800

    删除有序数组的重复项

---
+++ 26-删除有序数组中的重复项/test.py
@@ -0,0 +1,23 @@
+import unittest
+import remove_duplicates
+
+
+class MyTestCase(unittest.TestCase):
+    @staticmethod
+    def verify_function(nums, expected_nums):
+        length = remove_duplicates.my_solution1(nums)
+        if not length == len(expected_nums):
+            return False
+        for i in range(len(expected_nums)):
+            if not nums[i] == expected_nums[i]:
+                return False
+        return True
+
+    def test_solution1(self):
+        self.assertEqual(self.verify_function([1, 1, 2], [1, 2]), True)
+        self.assertEqual(self.verify_function([-1, 0, 0, 0, 0, 3, 3], [-1, 0, 3]), True)
+        self.assertEqual(self.verify_function([0, 0, 1, 1, 1, 2, 2, 3, 3, 4], [0, 1, 2, 3, 4]), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
